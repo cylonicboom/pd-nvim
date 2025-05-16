f2e682dc37b35be7c422aba7906adc258560afd1
Catherine Reprobate <catherine.reprobate@pm.me>
2025-05-16 11:18:23 -0700

    define default config for pd-nvim

---
+++ lazy.lua
@@ -0,0 +1,24 @@
+return {
+	{
+		'pd-nvim',
+		dependencies = {
+			-- which-key integration
+			{ "folke/which-key.nvim" },
+
+			-- Debugging tools
+			{ 'mfussenegger/nvim-dap' },
+			{ "julianolf/nvim-dap-lldb" },
+			{ 'folke/neodev.nvim' },
+			{ "rcarriga/nvim-dap-ui" },
+			{ "nvim-neotest/nvim-nio" },
+			{ "rcarriga/cmp-dap" },
+			{ "hrsh7th/nvim-cmp" },
+
+			-- Telescope extensions
+			{ 'nvim-telescope/telescope.nvim' },
+			{ "nvim-telescope/telescope-live-grep-args.nvim" },
+		},
+		config = true,
+		dev = true,
+	},
+}
